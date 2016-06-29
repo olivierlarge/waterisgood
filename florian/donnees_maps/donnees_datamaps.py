ec85c40c525d74d612ba41c00df179dff43e44dd
FlorianFirmin <florian.firmin@telecom-paristech.fr>
2016-06-29 12:49:45 +0200

    florian map mercredi V2

--- florian/donnees_maps/donnees_datamaps.py
+++ florian/donnees_maps/donnees_datamaps.py
@@ -3,7 +3,7 @@ import csv
 serie_pop=[]
 pop_country=[]
 
-with open("pop_data1990.csv","rU") as file:
+with open("water_improved_2005.csv","rU") as file:
 	next(file,None)
 	next(file,None)
 	next(file,None)
@@ -13,13 +13,13 @@ with open("pop_data1990.csv","rU") as file:
 		#print row[3]
 		pop_country=[]
 
-  		if (row[3]!=""):
+  		if (row[3]!="" and row[0]!="Total"):
   			pop_country.append(row[1])
-  			pop_country.append(1000*(float(row[3])+1))
+  			print (row[3])
+  			pop_country.append(float(row[3]))
   			serie_pop.append(pop_country)
 
 print (serie_pop)
 
-with open("serie_pop_1990.txt","w") as file2:
+with open("water_improved_2005.txt","w") as file2:
 	file2.write(str(serie_pop))
-
